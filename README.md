eb242662f5b3590351e3a2aa188ec88ad830dfa7
Paul1365972 <paul1365972@gmail.com>
2020-06-03 00:31:00 +0200

    Add exposed mysql datastore

---
+++ README.md
@@ -0,0 +1,2 @@
+# Metay
+Basic Paper plugin for attaching custom data to Blocks, Chunks, Worlds, Items, Players and Entities
---
+++ src/main/java/io/github/paul1365972/metay/storage/endpoints/ExposedDataStore.kt
@@ -0,0 +1,63 @@
+package io.github.paul1365972.metay.storage.endpoints
+
+import io.github.paul1365972.metay.storage.DataKey
+import io.github.paul1365972.metay.storage.MetayDataStore
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.statements.api.ExposedBlob
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class ExposedDataStore(
+        val database: Database,
+        tableName: String,
+        keyColumn: String = "key",
+        bytesColumn: String = "bytes"
+) : MetayDataStore<String> {
+
+    private val table = object : Table() {
+        val key = varchar(keyColumn, length = 127)
+        val bytes = blob(bytesColumn)
+
+        override val primaryKey = PrimaryKey(key)
+    }
+
+    init {
+        transaction(database) {
+            SchemaUtils.create(table)
+        }
+    }
+
+    override fun <T> get(dataKey: DataKey<T>, locationKey: String): T? {
+        val key = toKey(dataKey, locationKey)
+        return transaction(database) {
+            table.select {
+                table.key.eq(key)
+            }.firstOrNull()?.let {
+                dataKey.deserializer(it[table.bytes].bytes)
+            }
+        }
+    }
+
+    override fun <T> put(dataKey: DataKey<T>, locationKey: String, value: T) {
+        val key = toKey(dataKey, locationKey)
+        return transaction(database) {
+            table.replace {
+                it[table.key] = key
+                it[table.bytes] = ExposedBlob(dataKey.serializer(value))
+            }
+        }
+    }
+
+    override fun <T> remove(dataKey: DataKey<T>, locationKey: String) {
+        val key = toKey(dataKey, locationKey)
+        return transaction(database) {
+            table.deleteWhere {
+                table.key.eq(key)
+            }
+        }
+    }
+
+    private fun toKey(dataKey: DataKey<*>, locationKey: Any): String {
+        return "${dataKey.namespacedName}:${locationKey}"
+    }
+
+}
