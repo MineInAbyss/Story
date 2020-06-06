5ca102059e3e1aa0eb6398602732e5d0c367443a
Paul1365972 <paul1365972@gmail.com>
2020-06-07 01:50:19 +0200

    Improve API

--- src/main/java/io/github/paul1365972/metay/datastore/MetayDataStore.kt
+++ src/main/java/io/github/paul1365972/metay/datastore/MetayDataStore.kt
@@ -1,5 +1,8 @@
 package io.github.paul1365972.metay.datastore
 
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 interface MetayDataStore<L> {
 
     /**
@@ -12,9 +15,16 @@ interface MetayDataStore<L> {
      * @param locationKey Location of the value
      *
      * @return The deserialized value (that may be null) or null if the value is absent
-     *
      */
     fun <T> get(dataKey: DataKey<T>, locationKey: L): T?
+
+    /**
+     * @see [get]
+     * Uses the deserialized value or the default value, if the value is null or the data is missing
+     */
+    fun <T> get(dataKey: DataKey<T>, locationKey: L, defaultValue: (L) -> T): T {
+        return get(dataKey, locationKey) ?: defaultValue(locationKey)
+    }
 
     /**
      * Serializes the value and saves it.
@@ -46,7 +56,17 @@ interface MetayDataStore<L> {
      */
     fun <T> compute(dataKey: DataKey<T>, locationKey: L, block: (T?) -> T?): T? {
         val modified = block(get(dataKey, locationKey))
-        if (modified != null) put(dataKey, locationKey, modified) else remove(dataKey, locationKey)
+        set(dataKey, locationKey, modified)
+        return modified
+    }
+
+    /**
+     * @see [compute]
+     * Uses the deserialized value or the default value, if the value is null or the data is missing
+     */
+    fun <T> compute(dataKey: DataKey<T>, locationKey: L, defaultValue: (L) -> T, block: (T) -> T): T {
+        val modified = block(get(dataKey, locationKey) ?: defaultValue(locationKey))
+        put(dataKey, locationKey, modified)
         return modified
     }
 
@@ -60,13 +80,55 @@ interface MetayDataStore<L> {
      *
      * @return The updated value
      */
-    fun <T> update(dataKey: DataKey<T>, locationKey: L, block: DataSetter<T?>.(T?) -> Unit) {
+    fun <T> update(dataKey: DataKey<T>, locationKey: L, block: DataSetter<T?>.(T?) -> Unit): T? {
         val modified = DataSetter(get(dataKey, locationKey))
         block(modified, modified.get())
-        if (modified.hasChanged()) {
-            val value = modified.get()
-            if (value != null) put(dataKey, locationKey, value) else remove(dataKey, locationKey)
+        if (modified.hasChanged())
+            set(dataKey, locationKey, modified.get())
+        return modified.get()
+    }
+
+    /**
+     * @see [update]
+     * Uses the deserialized value or the default value, if the value is null or the data is missing
+     */
+    fun <T> update(dataKey: DataKey<T>, locationKey: L, defaultValue: (L) -> T, block: DataSetter<T>.(T) -> Unit): T {
+        val modified = DataSetter(get(dataKey, locationKey) ?: defaultValue(locationKey))
+        block(modified, modified.get())
+        if (modified.hasChanged())
+            set(dataKey, locationKey, modified.get())
+        return modified.get()
+    }
+
+    /**
+     * Provides a delegate to the value.
+     * Note: Be careful as the value is only [put] when assigning the value, only modifying its properties does not
+     *
+     * @param dataKey Key for the data
+     * @param locationKey Location of the value
+     *
+     * @return The value delegate
+     */
+    fun <T> access(dataKey: DataKey<T>, locationKey: L): ReadWriteProperty<Any?, T?> {
+        return object : ReadWriteProperty<Any?, T?> {
+            override fun getValue(thisRef: Any?, property: KProperty<*>): T? {
+                return get(dataKey, locationKey)
+            }
+            override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
+                set(dataKey, locationKey, value)
+            }
         }
+    }
+
+    /**
+     * Serializes the value and saves it, or removes it if value is null
+     *
+     * @param dataKey Key for the data
+     * @param locationKey Location of the value
+     * @param value Value to be stored or null to remove
+     */
+    fun <T> set(dataKey: DataKey<T>, locationKey: L, value: T?) {
+        if (value != null) put(dataKey, locationKey, value) else remove(dataKey, locationKey)
     }
 
     /**
--- src/main/java/io/github/paul1365972/metay/test/KtTest.kt
+++ src/main/java/io/github/paul1365972/metay/test/KtTest.kt
@@ -7,19 +7,18 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import org.bukkit.Location
 
-//TODO
+val blockStore = MemoryDataStore<Location>()
+
+@Serializable
+data class MagicData(
+        var a: Int,
+        var b: String
+)
+
+val magicKey = DataKey(null, "magic", MagicData.serializer(), Json(JsonConfiguration.Stable))
+
 fun test() {
     val loc = Location(null, 0.0, 0.0, 0.0)
-
-    val blockStore = MemoryDataStore<Location>()
-
-    @Serializable
-    data class MagicData(
-            var a: Int,
-            var b: String
-    )
-
-    val magicKey = DataKey(null, "magic", MagicData.serializer(), Json(JsonConfiguration.Stable))
 
     blockStore.compute(magicKey, loc) {
         (it ?: MagicData(0, "lorem")).apply {
@@ -32,4 +31,13 @@ fun test() {
         value.a += 3
         set(value)
     }
+
+    loc.magicData = (loc.magicData ?: MagicData(0, "lorem")).apply {
+        a += 3
+    }
+    loc.magicData!!.a = 3
 }
+
+var Location.magicData: MagicData?
+    get() = blockStore.get(magicKey, this)
+    set(value) = blockStore.set(magicKey, this, value)
