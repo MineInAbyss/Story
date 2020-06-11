62153164a463941341885cb2281effc0e64bb73d
Paul1365972 <paul1365972@gmail.com>
2020-06-12 00:09:26 +0200

    Fix equality bug

--- src/main/java/io/github/paul1365972/story/key/DataKey.kt
+++ src/main/java/io/github/paul1365972/story/key/DataKey.kt
@@ -18,8 +18,7 @@ abstract class DataKey<T : Any>(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as DataKey<*>
+        if (other !is DataKey<*>) return false
         if (namespacedName != other.namespacedName) return false
         return true
     }
